c6ccb447461f5933062555ca275e84bfc8ddf27d
NJPaz <nicole.j.paz@gmail.com>
2025-10-26 19:11:39 -0700

    Move fish component to separate file.

---
+++ app/components/FishCards.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { fishData } from "@/constants/fishData";
+import { FishDataType } from "@/constants/types";
+import clsx from "clsx";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+const wikiUrl = 'https://echoesoftheplumgrove.wiki.gg';
+
+function FishItem({ handleSetUserFishes, userFishes, fish }: { handleSetUserFishes: (fishName: string, target: HTMLElement) => void, userFishes: string[], fish: FishDataType }) {
+  const hasFish = useMemo(() => userFishes.indexOf(fish.name) !== -1, [userFishes]);
+
+  return (
+    <div className={clsx('card', 'pt-2', 'cursor-pointer', hasFish && 'selected')} role="button" onClick={(evt) => handleSetUserFishes(fish.name, evt.target as HTMLElement)}>
+      <img src={`${wikiUrl}${fish.imgLink}`} alt={`${fish.name} thumbnail`} />
+
+      <div className="card-content text-center">
+        <h2>{fish.name}</h2>
+        <a href={`${wikiUrl}${fish.link}`} target="_blank" rel="noopener noreferrer">More Info</a>
+      </div>
+    </div>
+  );
+}
+
+export default function FishCards() {
+  const [userFishes, setUserFishes] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fishes = JSON.parse(localStorage.getItem('fishes') as string);
+
+    if (fishes) {
+      setUserFishes(fishes);
+    }
+  }, [setUserFishes]);
+
+  const handleSetUserFishes = useCallback((fishName: string, target: HTMLElement) => {
+    if (target.tagName === 'A') {
+      // user has clicked the "More info" link
+      return;
+    }
+
+    let indexOfFish = userFishes.indexOf(fishName) ;
+
+    if (indexOfFish === -1) {
+      userFishes.push(fishName);
+    } else {
+      userFishes.splice(indexOfFish, 1);
+    }
+
+    localStorage.setItem('fishes', JSON.stringify(userFishes));
+
+    setUserFishes([...userFishes]);
+  }, [userFishes, setUserFishes]);
+
+  return fishData.map((fish: FishDataType) => <FishItem handleSetUserFishes={handleSetUserFishes} userFishes={userFishes} fish={fish} key={fish.link} />);
+}
